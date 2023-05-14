aff91eb8c22e6c9c02941f06529fa6112b7709a2
He Zhang <zhanghecool@outlook.com>
2023-05-14 20:25:22 +0800

    feat(new): add new index page style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,50 @@
+import { Button } from '@/components/ui/button';
+import { Github, Mail, Twitter } from 'lucide-react';
 import Image from 'next/image';
-// import { Camera } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono lg:flex">
-        <p className="fixed text-red-600 left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          CSS DAO
-          {/* <Camera size={48} /> */}
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://css.show"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/logo.svg"
-              alt="CSS DAO Logo"
-              // className="dark:invert"
-              width={30}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="relative">
+      <div className="bg-black w-full min-h-screen flex flex-col-reverse md:flex-row items-center justify-center p-4 md:p-8 lg:p-24 xl:p-32 2xl:p-48">
+        <div className='md:mr-4'>
+          <div className='flex items-center space-x-2 mb-4'>
+            <Link href='https://twitter.com/cssdao'>
+              <Button>
+                <Twitter className="mr-2 h-4 w-4" /> @cssdao
+              </Button>
+            </Link>
+            <Link href='https://github.com/cssdao'>
+              <Button>
+                <Github className="mr-2 h-4 w-4" /> @cssdao
+              </Button>
+            </Link>
+            <Link href='https://discord.gg/zRaDp3QvNu'>
+              <Button>
+                Discord
+              </Button>
+            </Link>
+          </div>
+          <h1 className='font-bold text-white text-[68px] leading-tight'>
+            <span className='text-[88px]'>CSS DAO</span><br />
+            开放、透明、<br />协作和创新。
+          </h1>
+        </div>
+        <div className='md:pl-[10%] md:ml-auto mb-6 md:mb-0'>
+          <Image
+            src="/logo.svg"
+            alt="CSS DAO"
+            className=""
+            width={400}
+            height={400}
+            priority
+          />
         </div>
       </div>
 
-      <div className="relative text-5xl text-[#274de4] font-bold flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative mr-6"
-          src="/logo.svg"
-          alt="CSS DAO Logo"
-          width={80}
-          height={37}
-          priority
-        />
-        Come soon...
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-
-      </div>
+      {/* <div className="relative p-4 md:p-8 lg:p-24">
+        
+      </div> */}
     </main>
   )
 }
