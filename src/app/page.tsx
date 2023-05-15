ea93df3f2583c9d447c67717de09668f412ba44d
He Zhang <zhanghecool@outlook.com>
2023-05-15 17:58:39 +0800

    feat(home): add what is cssdao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import { TypographyH1, TypographyInlineCode, TypographyP } from '@/components/ui/typography';
 import { Github, Mail, Twitter } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -21,7 +22,7 @@ export default function Home() {
             </Link>
             <Link href='https://discord.gg/zRaDp3QvNu'>
               <Button>
-                Discord
+                Discord @cssdao
               </Button>
             </Link>
           </div>
@@ -42,9 +43,15 @@ export default function Home() {
         </div>
       </div>
 
-      {/* <div className="relative p-4 md:p-8 lg:p-24">
-        
-      </div> */}
+      <div className="relative p-4 md:p-8 lg:p-24 xl:p-32 2xl:p-48">
+        <TypographyH1>CSS DAO 是什么？</TypographyH1>
+        <TypographyP>
+          CSS DAO 的愿景是成为一个在 Web3 世界中具有重要影响力的<TypographyInlineCode>技术性质 DAO</TypographyInlineCode>。<br />
+          我们致力于为 Web3 生态系统的发展做出贡献，通过技术创新和社区合作，推动 Web3 的应用和普及。<br /><br />
+          我们坚信，Web3 的未来是去中心化、安全、开放和透明的。因此，我们将致力于开发去中心化应用（DApps）、智能合约和其他 Web3 相关技术，为用户提供更加安全、私密和自主的在线体验。我们还将支持和参与 Web3 的标准化工作，以确保 Web3 生态系统的互操作性和可持续性。<br /><br />
+          CSS DAO 的核心价值观是开放、透明、协作和创新。我们欢迎各种背景和技术水平的人加入我们的社区，共同探索、学习和创造 Web3 的未来。我们相信，通过 CSS DAO 的努力和不断的创新，可以让 Web3 技术更加完善、普及，推动 Web3 世界的可持续发展。
+        </TypographyP>
+      </div>
     </main>
   )
 }
